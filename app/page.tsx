2c17c18f1e3427a2753589e7ca5b6b308f5e06fa
RedDotz20 <carlos_tabangay@outlook.com>
2023-08-11 15:20:25 +0800

    styles: navbar spacing | typography

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
 export default function Home() {
-	return (
-		<main className="bg-primaryColor flex min-h-screen flex-col items-center justify-between p-24">
-			<div className="bg-secondaryColor text-white">
-				FWDP MAIN WEBSITE
-			</div>
-		</main>
-	);
+  return (
+    <main className='flex min-h-screen flex-col items-center justify-between bg-primaryColor p-12'>
+      <h1>Filipino Web Development Peers</h1>
+    </main>
+  );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,34 +1,40 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 export default function Navbar() {
   return (
-    <header>
-      <nav>
-        <ul className="list-none m-0 p-8 flex gap-8 items-center text-xl text-white bg-midnightBlue">
-          <li>
-            <a href="#">
-              <Image
-                src={"/logo-horizontal.png"}
-                alt="FWDP logo"
-                width={250}
-                height={250}
-              />
-            </a>
-          </li>
-          <li className="ml-auto">
-            <a href="#">Team</a>
-          </li>
-          <li>
-            <a href="#">About</a>
-          </li>
-          <li>
-            <a href="#">Our Projects</a>
-          </li>
-          <li>
-            <a href="#">Join Us</a>
-          </li>
-        </ul>
-      </nav>
+    <header className='bg-midnightBlue px-8'>
+      <ul className='m-0 flex h-20 list-none items-center justify-center gap-8  text-xl text-white'>
+        <li>
+          <a href='#'>
+            <Image
+              src={'/logo-horizontal.png'}
+              alt='FWDP logo'
+              width={160}
+              height={160}
+            />
+          </a>
+        </li>
+        <li className='ml-auto'>
+          <a href='#' className='tracking-widest'>
+            TEAM
+          </a>
+        </li>
+        <li>
+          <a href='#' className='tracking-widest'>
+            ABOUT
+          </a>
+        </li>
+        <li>
+          <a href='#' className='tracking-widest'>
+            PROJECTS
+          </a>
+        </li>
+        <li>
+          <a href='#' className='tracking-wide'>
+            JOIN US
+          </a>
+        </li>
+      </ul>
     </header>
   );
 }
