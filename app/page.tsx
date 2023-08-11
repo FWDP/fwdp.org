678300210b59f73416ad1372fd3e2da416fc8938
genelorenzSarmiento0408 <sarmientoemmener83@gmail.com>
2023-08-11 11:30:38 +0800

    feat: create navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
-	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			FWDP MAIN WEBSITE
-		</main>
-	);
+  return (
+    <>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        FWDP MAIN WEBSITE
+      </main>
+    </>
+  );
 }
---
+++ components/Navbar.tsx
@@ -0,0 +1,13 @@
+export default function Navbar() {
+  return (
+    <header>
+      <nav>
+        <ul className="list-none m-0 p-16 text-white bg-fwdp-dark">
+          <li>
+            <a href="#">Home</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
