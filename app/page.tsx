89d0faa234d3b65685553603a660c22fa512a62a
gabrielleabasolo <gabrielleabasolo@gmail.com>
2023-08-11 11:33:32 +0800

    Color slates Changed

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			FWDP MAIN WEBSITE
+		<main className="bg-primaryColor flex min-h-screen flex-col items-center justify-between p-24">
+			<div className="bg-secondaryColor ">
+				FWDP MAIN WEBSITE
+			</div>
 		</main>
 	);
 }
