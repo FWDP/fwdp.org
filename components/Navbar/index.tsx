3d874e6ed779160ddfc03cccf395a096115b3693
RedDotz20 <carlos_tabangay@outlook.com>
2023-08-16 21:53:40 +0800

    styles: navbar structure

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,59 +1,39 @@
 'use client';
 
 import Image from 'next/image';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { IoCloseSharp } from 'react-icons/io5';
 import { RxHamburgerMenu } from 'react-icons/rx';
+
 import LinkRoutes from './LinkRoutes';
+import Link from 'next/link';
 
 export default function Navbar() {
   const [toggleMenu, setToggleMenu] = useState(false),
     toggleMenuNav = () => setToggleMenu(!toggleMenu);
 
-  const [screenWidth, setScreenWidth] = useState(0),
-    isMobile = screenWidth <= 768 && screenWidth !== 0;
-
-  useEffect(() => {
-    setScreenWidth(window.innerWidth);
-
-    const changeWidth = () => setScreenWidth(window.innerWidth);
-    window.addEventListener('resize', changeWidth);
-    return () => window.removeEventListener('resize', changeWidth);
-  }, []);
-
   return (
-    <nav className='items-center bg-primaryColor px-8'>
-      {!isMobile && <LinkRoutes isMobile={isMobile} />}
+    <nav className='relative flex items-center bg-primaryColor max-md:py-5'>
+      <div className='flex px-8 max-md:w-full'>
+        <Link href='/' className='mr-auto '>
+          <Image
+            src={'/logo-horizontal.png'}
+            alt='FWDP logo'
+            width={190}
+            height={190}
+          />
+        </Link>
 
-      {isMobile && (
-        <>
-          <div className='flex items-center'>
-            <Image
-              src={'/fwdpIcon.png'}
-              alt='FWDP-logo'
-              width={80}
-              height={80}
-              className='mr-auto'
-            />
+        <div className='ml-auto flex cursor-pointer items-center  md:hidden'>
+          {!toggleMenu ? (
+            <RxHamburgerMenu size={32} onClick={() => toggleMenuNav()} />
+          ) : (
+            <IoCloseSharp size={40} onClick={() => toggleMenuNav()} />
+          )}
+        </div>
+      </div>
 
-            {!toggleMenu ? (
-              <RxHamburgerMenu
-                size={32}
-                className='cursor-pointer'
-                onClick={() => toggleMenuNav()}
-              />
-            ) : (
-              <IoCloseSharp
-                size={40}
-                className='cursor-pointer'
-                onClick={() => toggleMenuNav()}
-              />
-            )}
-          </div>
-
-          {toggleMenu && <LinkRoutes isMobile={isMobile} />}
-        </>
-      )}
+      <LinkRoutes toggleMenu={toggleMenu} toggleMenuNav={toggleMenuNav} />
     </nav>
   );
 }
