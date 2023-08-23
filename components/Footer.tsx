4e503a436018ccfc3e7ef403b6fb84e1721bdbec
RedDotz20 <carlos_tabangay@outlook.com>
2023-08-23 11:50:55 +0800

    feat: footer add

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,43 @@
 import React from 'react';
+import { socialLink } from '../constants/socialLinks';
+
+import { IoLogoGithub } from 'react-icons/io5';
+import { TbBrandFacebookFilled } from 'react-icons/tb';
+import {
+  PiDiscordLogoFill,
+  PiLinkedinLogoFill,
+  PiYoutubeLogoFill,
+} from 'react-icons/pi';
+
+const Icons = [
+  <IoLogoGithub key={0} />,
+  <PiDiscordLogoFill key={1} />,
+  <PiLinkedinLogoFill key={2} />,
+  <TbBrandFacebookFilled key={3} />,
+  <PiYoutubeLogoFill key={4} />,
+];
 
 export default function Footer() {
-  return <footer className='h-[12.5rem] w-full bg-royalBlue'>Footer</footer>;
+  return (
+    <footer className='flex min-h-[12.5rem] w-full flex-col items-center gap-2 bg-royalBlue px-4 py-6 text-white'>
+      <span>SOCIALS</span>
+      <div className='my-auto flex flex-wrap justify-center gap-2'>
+        {socialLink.map((socialLink, index) => {
+          return (
+            <button
+              key={socialLink.label}
+              className='flex items-center justify-center gap-1 border border-white px-2 py-1 font-semibold uppercase tracking-wider'
+            >
+              {Icons[index]} {socialLink.name}
+            </button>
+          );
+        })}
+      </div>
+
+      <div className='mx-12 my-2 mt-auto h-1 w-full border-t-2 border-[#fff]'></div>
+      <p className='tracking-wide'>
+        Copyright © 2023 Filipino Web Development Peers. All Rights Reserved
+      </p>
+    </footer>
+  );
 }
